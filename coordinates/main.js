a870c2814aed734d48ad58b1b00e876417ab6e26
JH <67933778+jayhigh47@users.noreply.github.com>
2023-03-19 21:38:33 +0900

    JS: Coordinates and Scroll

---
+++ coordinates/main.js
@@ -0,0 +1,20 @@
+const horizontal = document.querySelector(".horizontal");
+const vertical = document.querySelector(".vertical");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  //console.log(`${x} ${y}`);
+
+  tag.innerHTML = `${x}px, ${y}px`;
+
+  horizontal.style.top = `${y}px`;
+  vertical.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+});
